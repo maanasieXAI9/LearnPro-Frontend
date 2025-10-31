f6d73be582fbbeedadacc6e531b8435498c05349
maanasieXAI9 <maanasiexai9@gmail.com>
2025-10-31 22:50:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 
-export const server = "https://learnpro-server-jolx.onrender.com/";
+export const server = "https://learnpro-server-jolx.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
